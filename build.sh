6ed4dc7640ba0d0502e2dfd4d6b3da667a7ba93b
johnmart19 <johnivan19999@gmail.com>
2021-03-21 07:34:02 +0200

    Update Buildtools to latest one

--- build.sh
+++ build.sh
@@ -1,85 +1,60 @@
-#!/usr/bin/env bash
-# SPDX-License-Identifier: GPL-3.0-or-later
-# Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
-# Copyright (C) 2018 Rama Bondan Prakoso (rama982)
-# Android Kernel Build Script
+#!/bin/bash
+#
+# Compile script for Cartel kernel
+# Copyright (C) 2020-2021 Adithya R & johnmart19.
 
-# Main environtment
-KERNEL_DIR=$PWD
-KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
-ZIP_DIR=$KERNEL_DIR/AnyKernel3
-CONFIG=onc_defconfig
+SECONDS=0 # builtin bash timer
+ZIPNAME="Cartel-onclite-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$HOME/tc/proton-clang"
+DEFCONFIG="onc_defconfig"
 
-# Export
-export ARCH=arm64
-export CROSS_COMPILE
-export CROSS_COMPILE_ARM32
+export PATH="$TC_DIR/bin:$PATH"
 
-# Main Staff
-
-clang_bin="$HOME/toolchains/proton-clang/bin"
-gcc_prefix64="aarch64-linux-gnu-"
-gcc_prefix32="arm-linux-gnueabi-"
-CROSS_COMPILE="aarch64-linux-gnu-"
-CROSS_COMPILE_ARM32="arm-linux-gnueabi-"
-
-_ksetup_old_path="$PATH"
-export PATH="$clang_bin:$PATH"
-
-# Build start
-make	O=out $CONFIG
-make	\
-	O=out \
-	ARCH=arm64 \
-	CC="ccache clang" \
-	AR=llvm-ar \
-	NM=llvm-nm \
-	LD=ld.lld \
-	STRIP=llvm-strip \
-	OBJCOPY=llvm-objcopy \
-	OBJDUMP=llvm-objdump \
-	OBJSIZE=llvm-size \
-	READELF=llvm-readelf \
-	HOSTCC=clang \
-	HOSTCXX=clang++ \
-	HOSTAR=llvm-ar \
-	HOSTLD=ld.lld \
-	CROSS_COMPILE=aarch64-linux-gnu- \
-	CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-	-j`nproc --all`
-
-if ! [ -a $KERN_IMG ]; then
-    echo "Build error!"
-    exit 1
+if ! [ -d "$TC_DIR" ]; then
+echo "Proton clang not found! Cloning to $TC_DIR..."
+if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang $TC_DIR; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
 fi
 
-cd $ZIP_DIR
-make clean &>/dev/null
+export KBUILD_BUILD_USER=johnmart19
+export KBUILD_BUILD_HOST=Johnmart19-PC
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+if [[ $1 == "-r" || $1 == "--regen" ]]; then
+cp out/.config arch/arm64/configs/$DEFCONFIG
+echo -e "\nRegened defconfig succesfully!"
+exit 0
+else
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
+fi
+
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
+echo -e "\nKernel compiled succesfully! Zipping up...\n"
+if ! [ -d "AnyKernel3" ]; then
+echo "AnyKernel3 not found! Cloning..."
+if ! git clone -q --depth=1 --single-branch https://github.com/johnmart19/AnyKernel3 -b redline_onclite AnyKernel3; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+cp out/arch/arm64/boot/dtbo.img AnyKernel3
+rm -f *zip
+cd AnyKernel3
+zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
-
-# For MIUI Build
-# Credit Adek Maulana <adek@techdro.id>
-OUTDIR="$KERNEL_DIR/out/"
-VENDOR_MODULEDIR="$KERNEL_DIR/AnyKernel3/modules/vendor/lib/modules"
-
-STRIP="$HOME/toolchains/proton-clang/aarch64-linux-gnu/bin/strip$(echo "$(find "$HOME/toolchains/proton-clang/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
-            sed -e 's/gcc/strip/')"
-for MODULES in $(find "${OUTDIR}" -name '*.ko'); do
-    "${STRIP}" --strip-unneeded --strip-debug "${MODULES}"
-    "${OUTDIR}"/scripts/sign-file sha512 \
-            "${OUTDIR}/certs/signing_key.pem" \
-            "${OUTDIR}/certs/signing_key.x509" \
-            "${MODULES}"
-    find "${OUTDIR}" -name '*.ko' -exec cp {} "${VENDOR_MODULEDIR}" \;
-done
-cd libufdt/src && python2 mkdtboimg.py create $OUTDIR/arch/arm64/boot/dtbo.img $OUTDIR/arch/arm64/boot/dts/qcom/*.dtbo
-echo -e "\n(i) Done moving modules"
-
-cd $ZIP_DIR
-cp $KERN_IMG zImage
-cp $OUTDIR/arch/arm64/boot/dtbo.img $ZIP_DIR
-make normal &>/dev/null
-echo "Flashable zip generated under $ZIP_DIR."
-cd ..
-# Build end
-
+echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+if command -v gdrive &> /dev/null; then
+gdrive upload --share $ZIPNAME
+else
+echo "Zip: $ZIPNAME"
+fi
+rm -rf out/arch/arm64/boot
+else
+echo -e "\nCompilation failed!"
+fi
